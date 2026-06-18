031466d439c2a1b41640ad1c7633f2b63152e069
ibrohim-701 <ibrohimmukhiddinov0@gmail.com>
2026-06-18 10:08:27 +0500

    ibrohim22

--- src/components/UseFetch.jsx
+++ src/components/UseFetch.jsx
@@ -4,7 +4,7 @@ function UseFetch() {
   return (
     <div>
       <h1>branch 2</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi illo cumque unde eligendi hic impedit, culpa vitae veniam eveniet repellat enim recusandae dolorum repudiandae tempora earum magnam, iure ad nemo.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi illo cumque unde eligendi hic impedit, culpa vitae veniam eveniet repellat enim recusandae dolorum repudiandae tempora earum magnam, iure ad nemo. s</p>
     </div>
   )
 }
